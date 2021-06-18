e2fd84eb3650bf4349318fa32290b53f278bd229
buddy <you@example.com>
2021-06-18 19:06:51 +0530

    onClick images added

--- rock_paper_scissor/app.js
+++ rock_paper_scissor/app.js
@@ -7,108 +7,122 @@ const computerScore_span = document.getElementById("computer_score");
 const scoreboard_div = document.querySelector(".score-board");
 const result_Div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p")
-const scissors_div = document.getElementById("s")
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+const resut_print = document.querySelector(".results");
 
-
-//  rock = 1 
+//  rock = 1
 //  paper = 2
-//  scissors = 3 
+//  scissors = 3
 
-var human=0;
-var robo=0;
-
-
-function randomNumber(min=1, max=3) { 
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-} 
-
-function win (){
-    userScore++;
-    userScore_span.innerHTML = userScore;
+function randomNumber(min = 1, max = 3) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function lose(){
-    computerScore++;
-    computerScore_span.innerHTML = computerScore;
+function win() {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  resut_print.innerHTML ="  <p>You Won</p> ";
 }
 
-function draw(){
-    userScore++;
-    computerScore++;
-    computerScore_span.innerHTML = computerScore;
-    userScore_span.innerHTML = userScore;
-    
-}
-
-function game(userChoice){
-    const machine = randomNumber();
-    console.log(machine);
-    console.log("user -> " + userChoice);
-
-
-    if(machine == userChoice == 1){
-        draw();
-    }
-
-    if(machine == 1 && userChoice == 2){
-        win();
-    }
-
-    if(machine == 1 && userChoice == 3){
-        lose();
-    }
-
-    if(machine == userChoice == 2){
-        draw();
-    }
-
-    if(machine == 2 && userChoice == 1){
-        lose();
-    }
-
-    if(machine == 2 && userChoice == 3){
-        win();
-    }
-
-    if(machine == userChoice == 3){
-        draw();
-    }
-
-    if(machine == 3 && userChoice == 1){
-        win();
-    }
-
-    if(machine == 3 && userChoice == 2){
-        lose();
-    }
+function lose() {
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  resut_print.innerHTML ="  <p>You lose</p> ";
 
 }
 
-function main(){
+function draw() {
+  userScore++;
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  userScore_span.innerHTML = userScore;
+  resut_print.innerHTML ="  <p>Draw</p> ";
 
-    rock_div.addEventListener('click', (rock) => {
-        game(1);
-        // console.log(human);
-    })
-    
-    paper_div.addEventListener('click', (paper) => {
-        game(2);
-        // console.log(human);
-
-    })
-    
-    scissors_div.addEventListener('click', (sci) => {
-        game(3);
-        // console.log(human);
-
-    })
-    
 }
 
+function game(userChoice) {
+  const machine = randomNumber();
 
+  var src = document.getElementById("resulti");
 
+  if (machine == 1) {
+    src.innerHTML = ' <img src="assets/rock.png">';
+  }
+
+  if (machine == 2) {
+    src.innerHTML = ' <img src="assets/paper.png">';
+  }
+
+  if (machine == 3) {
+    src.innerHTML = ' <img src="assets/scissors.png">';
+  }
+
+  if ((machine == userChoice) == 1) {
+    draw();
+  }
+
+  if (machine == 1 && userChoice == 2) {
+    win();
+  }
+
+  if (machine == 1 && userChoice == 3) {
+    lose();
+  }
+
+  if ((machine == userChoice) == 2) {
+    draw();
+  }
+
+  if (machine == 2 && userChoice == 1) {
+    lose();
+    resut_print.innerHTML = "  <p>You lose</p> ";
+  }
+
+  if (machine == 2 && userChoice == 3) {
+    win();
+  }
+
+  if ((machine == userChoice) == 3) {
+    draw();
+  }
+
+  if (machine == 3 && userChoice == 1) {
+    win();
+  }
+
+  if (machine == 3 && userChoice == 2) {
+    lose();
+  }
+}
+
+function main() {
+  rock_div.addEventListener("click", () => {
+    game(1);
+
+    var src = document.getElementById("resultHuman");
+
+    src.innerHTML = ' <img src="assets/rock.png">';
+  });
+
+  paper_div.addEventListener("click", () => {
+    game(2);
+
+    var src = document.getElementById("resultHuman");
+
+    src.innerHTML = ' <img src="assets/paper.png">';
+  });
+
+  scissors_div.addEventListener("click", () => {
+    game(3);
+    // console.log(human);
+
+    var src = document.getElementById("resultHuman");
+
+    src.innerHTML = ' <img src="assets/scissors.png">';
+  });
+}
 
 main();
